4da2ea352abeae3224d7d0a655fc368c81da61f7
AyanKhurt <ayankhurtgmail.com>
2025-09-23 14:47:12 -0700

    new

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App.js";
-  import "./index.css";
+import { createRoot } from "react-dom/client";
+import App from "./App.js";
+import "./index.css";
 import { StrictMode } from "react";
 
-  createRoot(document.getElementById("root")).render(
-<StrictMode>
-<App />
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
 
-</StrictMode>
+  </StrictMode>
 
-  );
-  
+);
